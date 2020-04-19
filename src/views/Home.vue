a6351477dbe9adc0cf2848a53142742e1d114e70
XMRHRX <mrhr12345@gmail.com>
2020-04-19 19:36:16 +0800

    can work...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+	<input type="text" v-model="locationName" placeholder="請輸入地點">
+	<button @click="local">Show location</button>
+	<h1>
+	{{message}}
+	</h1>
+	<pre>{{result}}</pre>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      message: "",
+      locationName: "",
+	  result: "",
+    };
+  },
+  methods: {
+    getMessage() {
+      return "Hi I am a sync message";
+    },
+    asyncMessage() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("I am an async message");
+        }, 1000);
+      }).then((res) => {
+        this.message = res;
+      });
+    },
+    local() {
+      let api = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=rdec-key-123-45678-011121314";
+	  if(this.locationName == "")
+	  {
+	  	alert("please input location");
+	  	return;
+	  }
+	  api+=`&locationName=${this.locationName}`;
+      return this.axios.get(api).then((res) => {
+        let weatherData = res.data['records']['location']['0']['weatherElement'];
+		this.result="";
+		for(var row in weatherData)
+		{
+			this.result += weatherData[row]["elementName"]+":"+weatherData[row]['elementValue'] + "\n";
+		}
+        /*this.result = ;*/
+      });
+    }
   }
 };
 </script>
