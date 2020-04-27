3057c849219874ef55dcbc3aa1182c54d983e4ee
yaotsung <er0106er@gmail.com>
2020-04-28 01:20:36 +0800

    Import Buefy and update search way in about page
    import Buefy
    search way is kindness to users now in about page
    add prettierrc
    format all

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,152 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <section>
+        <b-select
+          placeholder="Select a city"
+          v-model="city"
+          v-on:input="findblock()"
+        >
+          <option v-for="city in cities" :key="city">
+            {{ city.name }}
+          </option>
+        </b-select>
+        <b-select
+          placeholder="Select a block"
+          v-model="block"
+          v-on:input="findlocation()"
+        >
+          <option v-for="block in blocks" :key="block">
+            {{ block }}
+          </option>
+        </b-select>
+        <b-select
+          placeholder="Select a location"
+          v-model="location"
+          v-on:input="output()"
+        >
+          <option v-for="location in locations" :key="location">
+            {{ location }}
+          </option>
+        </b-select>
+      </section>
+      <h1>{{ location }}</h1>
+      <h2>溫度:{{ temp }}</h2>
+      <h3>經緯度:{{ lat }},{{ lot }}</h3>
+      <h3>觀測時間:{{ time }}</h3>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      time: '',
+      lat: '',
+      lot: '',
+      temp: '',
+      locations: [],
+      blocks: [],
+      api:
+        'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=rdec-key-123-45678-011121314',
+      cities: [
+        { name: '基隆市' },
+        { name: '臺北市' },
+        { name: '新北市' },
+        { name: '桃園市' },
+        { name: '新竹市' },
+        { name: '新竹縣' },
+        { name: '苗栗縣' },
+        { name: '臺中市' },
+        { name: '彰化縣' },
+        { name: '南投縣' },
+        { name: '雲林縣' },
+        { name: '嘉義市' },
+        { name: '嘉義縣' },
+        { name: '臺南市' },
+        { name: '高雄市' },
+        { name: '屏東縣' },
+        { name: '宜蘭縣' },
+        { name: '花蓮縣' },
+        { name: '臺東縣' },
+        { name: '澎湖縣' },
+        { name: '金門縣' },
+        { name: '連江縣' }
+      ]
+    }
+  },
+  methods: {
+    findblock() {
+      this.blocks = []
+      this.locations = []
+      this.axios.get(this.api).then((res) => {
+        for (let n = 0; n <= 438; n++) {
+          if (
+            this.city ==
+            res.data['records']['location'][n]['parameter']['0'][
+              'parameterValue'
+            ]
+          ) {
+            if (
+              this.blocks.indexOf(
+                res.data['records']['location'][n]['parameter']['2'][
+                  'parameterValue'
+                ]
+              ) == -1
+            )
+              this.blocks.push(
+                res.data['records']['location'][n]['parameter']['2'][
+                  'parameterValue'
+                ]
+              )
+          }
+        }
+      })
+    },
+    findlocation() {
+      this.locations = []
+      this.axios.get(this.api).then((res) => {
+        for (let n = 0; n <= 438; n++) {
+          if (
+            this.block ==
+            res.data['records']['location'][n]['parameter']['2'][
+              'parameterValue'
+            ]
+          ) {
+            if (
+              this.city ==
+              res.data['records']['location'][n]['parameter']['0'][
+                'parameterValue'
+              ]
+            ) {
+              this.locations.push(
+                res.data['records']['location'][n]['locationName']
+              )
+            }
+          }
+        }
+      })
+    },
+    output() {
+      this.message = ''
+      this.axios.get(this.api).then((res) => {
+        for (let n = 0; n <= 438; n++) {
+          if (
+            this.location == res.data['records']['location'][n]['locationName']
+          ) {
+            this.time = res.data['records']['location'][n]['time']['obsTime']
+            this.lat = res.data['records']['location'][n]['lat']
+            this.lot = res.data['records']['location'][n]['lat']
+            this.temp =
+              res.data['records']['location'][n]['weatherElement']['3'][
+                'elementValue'
+              ]
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,60 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-	<input type="text" v-model="locationName" placeholder="請輸入地點">
-	<button @click="local">Show location</button>
-	<h1>
-	{{message}}
-	</h1>
-	<pre>{{result}}</pre>
+    <input type="text" v-model="locationName" placeholder="請輸入地點" />
+    <button @click="local">Show location</button>
+    <h1>
+      {{ message }}
+    </h1>
+    <pre>{{ result }}</pre>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: "Home",
+  name: 'Home',
   data() {
     return {
-      message: "",
-      locationName: "",
-	  result: "",
-    };
+      message: '',
+      locationName: '',
+      result: ''
+    }
   },
   methods: {
     getMessage() {
-      return "Hi I am a sync message";
+      return 'Hi I am a sync message'
     },
     asyncMessage() {
       return new Promise((resolve) => {
         setTimeout(() => {
-          resolve("I am an async message");
-        }, 1000);
+          resolve('I am an async message')
+        }, 1000)
       }).then((res) => {
-        this.message = res;
-      });
+        this.message = res
+      })
     },
     local() {
-      let api = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=rdec-key-123-45678-011121314";
-	  if(this.locationName == "")
-	  {
-	  	alert("please input location");
-	  	return;
-	  }
-	  api+=`&locationName=${this.locationName}`;
+      let api =
+        'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=rdec-key-123-45678-011121314'
+      if (this.locationName == '') {
+        alert('please input location')
+        return
+      }
+      api += `&locationName=${this.locationName}`
       return this.axios.get(api).then((res) => {
-        let weatherData = res.data['records']['location']['0']['weatherElement'];
-		this.result="";
-		for(var row in weatherData)
-		{
-			this.result += weatherData[row]["elementName"]+":"+weatherData[row]['elementValue'] + "\n";
-		}
+        let weatherData = res.data['records']['location']['0']['weatherElement']
+        this.result = ''
+        for (var row in weatherData) {
+          this.result +=
+            weatherData[row]['elementName'] +
+            ':' +
+            weatherData[row]['elementValue'] +
+            '\n'
+        }
         /*this.result = ;*/
-      });
+      })
     }
   }
-};
+}
 </script>
